a3fa7802ae392b2acf421c1690a0176fe5dabc83
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-10-27 14:28:12 -0600

    Added comments

--- app/src/main/java/com/example/guessinggame/Guess.java
+++ app/src/main/java/com/example/guessinggame/Guess.java
@@ -2,28 +2,41 @@ package com.example.guessinggame;
 
 public class Guess {
 
+    // Define the total number of guesses
     int guesses = 3;
 
+    // Create a returnable string
     public String checkGuess(int userGuess, int randomNumber) {
-        System.out.println("NOG: " + guesses);
-        if(guesses >= 0) {
+        // Check if the guess is less than 0
+        if(guesses > 0) {
+            // Check if the user guess is less than the random number
             if (userGuess < randomNumber) {
-                System.out.println("A");
+                // Remove the total number of guesses left
                 guesses--;
+
+                // Inform the user their guess was too low and how many they have left
                 return "Your guess was too low!\n You now have " + guesses + " left!";
-            } else if (userGuess > randomNumber) {
-                System.out.println("B");
+            }
+            // Check if the users guess is higher than the random number.
+            else if (userGuess > randomNumber) {
+                // Remove the total number of guesses left
                 guesses--;
+                // Inform the user their guess is too high and how many they have left
                 return "Your guess was too high!\n You now have " + guesses + " left!";
             } else {
-                System.out.println("C");
+                // Set the count to negative one.
+                guesses = -1;
+
+                // Inform the user that their guess was perfect!
                 return "Your guess was perfect!";
             }
         } else {
+            // Yell at them that they're out of guesses!
             return "You are out of guesses!";
         }
     }
 
+    // Generate a random number
     public int generateRandomNumber(int minValue, int maxValue) {
         return (int) Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
     }
--- app/src/main/java/com/example/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.guessinggame;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,7 +11,10 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Globally define the guess class
     Guess guess = new Guess();
+    // Globally define a variable to store the random number
     int randomNumber;
 
     @Override
@@ -19,13 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    // User submission button interaction
     public void btnGuessInteract(View view) {
+        // Grab the users guess, output view, and submission button
         EditText getUserGuess = findViewById(R.id.txtUserGuess);
+        TextView guessOutput = findViewById(R.id.txtOutput);
+        Button getBtn = (Button) findViewById(R.id.btnGuess);
 
-        System.out.println("FIRST GUESSES: " + guess.guesses);
-
+        // Create an integer to store the users guess
         int userGuess;
+
+        // Check if the guess counter is greater than 0
         if(guess.guesses > 0) {
             // Check if the text box is empty
             if (TextUtils.isEmpty(getUserGuess.getText().toString())) {
@@ -33,30 +41,39 @@ public class MainActivity extends AppCompatActivity {
                 getUserGuess.setError("Please enter a number");
                 return;
             } else {
-                // Otherwise grab the number and parse it into a double
+                // Otherwise grab the number and parse it into an integer
                 userGuess = Integer.parseInt(getUserGuess.getText().toString());
-
                 // Remove the error message
                 getUserGuess.setError(null);
             }
-
-            TextView guessOutput = findViewById(R.id.txtOutput);
+            // Use the guess class to check their guess parsing the guessed number and the random number itself/
             guessOutput.setText(guess.checkGuess(userGuess, randomNumber));
         }
+        // Otherwise..
         else {
+            // Disable the ability for a user to submit a guess or use the button
             getUserGuess.setEnabled(false);
-
-            Button getBtn = (Button) findViewById(R.id.btnGuess);
             getBtn.setEnabled(false);
+
+            // Tell the user they are out of guesses.
+            guessOutput.setText("You are out of guesses!");
 
         }
     }
 
+    // Random number button interaction
     public void btnRandomNumberInteract(View view) {
+        // Define the minimum value.
         int minValue;
+        // Define the maximum value.
         int maxValue;
 
+        // Grab both the minimum and maximum values
         EditText getMinValue = findViewById(R.id.txtMinimum);
+        EditText getMaxValue = findViewById(R.id.txtMaximum);
+
+        // Grab the output box to alert the invidual when the number is generated
+        TextView randomNumberGenerated = findViewById(R.id.txtRandomGenerated);
 
         // Check if the text box is empty
         if (TextUtils.isEmpty(getMinValue.getText().toString())) {
@@ -64,29 +81,37 @@ public class MainActivity extends AppCompatActivity {
             getMinValue.setError("Please enter a number");
             return;
         } else {
-            // Otherwise grab the number and parse it into a double
+            // Otherwise grab the number and parse it into a integer
             minValue = Integer.parseInt(getMinValue.getText().toString());
-            System.out.println(minValue);
             // Remove the error message
             getMinValue.setError(null);
         }
 
-        EditText getMaxValue = findViewById(R.id.txtMaximum);
         // Check if the text box is empty
         if (TextUtils.isEmpty(getMaxValue.getText().toString())) {
             // Define the error
             getMaxValue.setError("Please enter a number");
             return;
         } else {
-            // Otherwise grab the number and parse it into a double
+            // Otherwise grab the number and parse it into a integer
             maxValue = Integer.parseInt(getMaxValue.getText().toString());
-            System.out.println(maxValue);
             // Remove the error message
             getMaxValue.setError(null);
         }
 
-        TextView randomNumberGenerated = findViewById(R.id.txtRandomGenerated);
-        randomNumber = guess.generateRandomNumber(minValue, maxValue);
-        randomNumberGenerated.setText("A random number was generated!");
+        // Check if the minimum number is greater than the maximum value, or if it is equal to the maximum value
+        if(minValue > maxValue || minValue == maxValue) {
+            // Change the text color
+            randomNumberGenerated.setTextColor(Color.RED);
+            // Request that they provide another set of numbers.
+            randomNumberGenerated.setText("Please select a smaller minimum number!");
+        } else {
+            // Assign the random number a random generated number in an inclusive range
+            randomNumber = guess.generateRandomNumber(minValue, maxValue);
+            // Set the text color back to grey.
+            randomNumberGenerated.setTextColor(Color.DKGRAY);
+            // Tell the user that the random number has been generated
+            randomNumberGenerated.setText("A random number was generated!");
+        }
     }
 }
